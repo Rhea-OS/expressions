796d0aebec6f2f41f4331788458a5b9d96f44254
set <set>
2024-09-26 09:29:50 +0200

    Rust Config

---
+++ src/error.rs
@@ -0,0 +1,21 @@
+use std::fmt::Display;
+use std::fmt::Debug;
+use std::fmt::Formatter;
+use std::error::Error as Err;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(&self, f)
+    }
+}
+
+impl Err for Error {
+
+}
