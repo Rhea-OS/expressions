c43f124c7f7058d7769aa1d17dc8309cc58821ef
JCake <jakieschneider13@gmail.com>
2024-10-12 11:43:55 +0200

    Implement remaining constructs

--- src/parse/test.rs
+++ src/parse/test.rs
@@ -3,11 +3,12 @@ pub mod test {
     use crate::error::*;
     use crate::parse::parse;
 
+    use crate::parse::call::Call;
     use crate::parse::expression::Expression;
+    use crate::parse::key::Key;
     use crate::parse::literal::Literal;
     use crate::parse::*;
-    use crate::parse::call::Call;
-    use crate::parse::key::Key;
+    use crate::parse::associative_array::AssociativeArray;
     use crate::parse::list::List;
 
     #[test]
@@ -53,10 +54,63 @@ pub mod test {
     }
 
     #[test]
+    pub fn test_parentheses() -> Result<()> {
+        let expr = parse("1*(2+3)^4");
+
+        assert_eq!(expr?, Value::Expression(Expression {
+            operands: vec![
+                Value::Literal(Literal::Number(1.0)),
+                Value::Expression(Expression {
+                    operands: vec![
+                        Value::Expression(Expression {
+                            operands: vec![
+                                Value::Literal(Literal::Number(2.0)),
+                                Value::Literal(Literal::Number(3.0)),
+                            ],
+                            operator: "+"
+                        }),
+                        Value::Literal(Literal::Number(4.0)),
+                    ],
+                    operator: "^"
+                })
+            ],
+            operator: "*"
+        }));
+
+        Ok(())
+    }
+
+    #[test]
     pub fn test_call() -> Result<()> {
         assert_eq!(parse("hello(1)")?, Value::Call(Call {
             name: Key::Name("hello".to_owned()),
             arguments: vec![Value::Literal(Literal::Number(1.0))],
+        }));
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_list() -> Result<()> {
+        assert_eq!(parse("[1,2,3]")?, Value::List(List {
+            items: vec![
+                Value::Literal(Literal::Number(1.0)),
+                Value::Literal(Literal::Number(2.0)),
+                Value::Literal(Literal::Number(3.0)),
+            ]
+        }));
+
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_associative_array() -> Result<()> {
+        assert_eq!(parse("[tomato=1,beans=2,cheese=3]")?, Value::AssociativeArray(AssociativeArray {
+            items: vec![
+                (Key::Name("tomato".into()), Value::Literal(Literal::Number(1.0))),
+                (Key::Name("beans".into()), Value::Literal(Literal::Number(2.0))),
+                (Key::Name("cheese".into()), Value::Literal(Literal::Number(3.0))),
+            ]
         }));
 
         Ok(())
