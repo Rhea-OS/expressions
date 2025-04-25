150e12857537c93cf3a65dffd715fd2b65844896
jcake <jakieschneider13@gmail.com>
2025-04-25 14:49:22 +0200

    JS docs plus manual types

--- expression-js/build.js
+++ expression-js/build.js
@@ -3,56 +3,45 @@ import cp from "node:child_process";
 import {esbuild} from 'builder';
 
 export default {
-    async "build:main.wasm"(config) {
-        const out = config.out.join("mod");
-        const wasmPack = cp.spawn('wasm-pack', [
-            'build',
-            '--out-dir',
-            out.path,
-        ], {
-            stdio: "inherit"
-        });
-        await new Promise((ok, err) => wasmPack.once("exit", code => code === 0 ? ok() : err(code)));
-    },
-    async "build:main.js"(config) {
-        await esbuild.build({
-            entryPoints: ["lib.ts"],
-            bundle: true,
-            sourcemap: true,
-            format: 'esm',
-            loader: {
-                ".wasm": "binary"
-            },
-            outdir: config.out.join("pkg").path
-        });
-    },
-    async "build:lib.d.ts"(config) {
-        await fs.copyFile(config.out.join("mod/expression_js.d.ts").path, config.out.join("pkg/lib.d.ts").path);
-    },
-    async "build:package.json"(config) {
-        await fs.writeFile(config.out.join("pkg/package.json").path, JSON.stringify({
-            name: "expression",
-            type: "module",
-            main: "./lib.js",
-            typings: "./lib.d.ts"
-        }, null, 4));
-    },
-    async "test:lib.js"(config) {
-        const file = config.out.join("lib.js");
-        /**
-         * @type {typeof import('./build/mod/expression_js.d.ts')}
-         */
-        const mod = await import(file.path).then(mod => mod.default());
+	async "build:main.wasm"(config) {
+		const out = config.out.join("mod");
+		const wasmPack = cp.spawn('wasm-pack', [
+			'build',
+			'--out-dir',
+			out.path,
+		], {
+			stdio: "inherit"
+		});
+		await new Promise((ok, err) => wasmPack.once("exit", code => code === 0 ? ok() : err(code)));
+	},
+	async "build:main.js"(config) {
+		await esbuild.build({
+			entryPoints: ["lib.ts"],
+			bundle: true,
+			sourcemap: true,
+			format: 'esm',
+			loader: {
+				".wasm": "binary"
+			},
+			outdir: config.out.join("pkg").path
+		});
+	},
+	async "build:lib.d.ts"(config) {
+		const lib = await fs.readFile('./lib.d.ts', {encoding: 'utf8'});
+		const mod = await fs.readFile(config.out.join("mod/expression_js.d.ts").path, {encoding: 'utf8'});
 
-        const cx = new mod.Context(new mod.DataSource({
-            listRows: () => [],
-            listColumns: () => [],
-            countRows: () => 0,
-            getRow: row => void 0
-        }));
+		await fs.writeFile(config.out.join("pkg/expr.d.ts").path, mod);
 
-        cx.pushGlobal("x", "Hello")
-
-        console.log(cx.evaluate(`x+x`));
-    }
+		await fs.writeFile(config.out.join("pkg/lib.d.ts").path, lib
+			.replaceAll(lib.match(/( {2,})/)[0], '\t')
+			.replaceAll(/\n{2,}/g, '\n'));
+	},
+	async "build:package.json"(config) {
+		await fs.writeFile(config.out.join("pkg/package.json").path, JSON.stringify({
+			name: "expression",
+			type: "module",
+			main: "./lib.js",
+			typings: "./lib.d.ts"
+		}, null, 4));
+	}
 }
